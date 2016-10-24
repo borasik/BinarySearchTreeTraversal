57cc1353a3cb70ba7b4edbbd623df41e2c4c5c8d
Alex Borsuk <alex.borsuk@dh.com>
2016-10-24 18:57:47 -0400

    BuildTree Finished

--- BinarySearchTreeTraversal/Program.cs
+++ BinarySearchTreeTraversal/Program.cs
@@ -10,7 +10,73 @@ namespace BinarySearchTreeTraversal
     {
         static void Main(string[] args)
         {
+            var array = new List<int> {2, 5, 50, 1 , 0, 240, 33, 0 };
 
+            BinarySearchTree bst = new BinarySearchTree();
+            foreach(var a in array)
+            {
+                bst.BuildTree(null, a);
+            }
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        private Node<int> RootNode { get; set; }
+
+        public void BuildTree(Node<int> node, int value)
+        {
+            if(RootNode == null)
+            {
+                RootNode = new Node<int>(value);
+            }
+            else
+            {
+                AddNode(RootNode, value);
+            }
+        }
+
+        public void AddNode(Node<int> node, int value)
+        {
+            if (value <= node.Value)
+            {
+                if (node.LeftChild != null)
+                {
+                    AddNode(node.LeftChild, value);
+                }
+                else
+                {
+                    node.LeftChild = new Node<int>(value);
+                }
+            }
+
+            if (value > node.Value)
+            {
+                if (node.RightChild != null)
+                {
+                    AddNode(node.RightChild, value);
+                }
+                else
+                {
+                    node.RightChild = new Node<int>(value);
+                }
+            }
+        }
+
+        public void InOrderTraversal()
+        {
+
+        }
+    }    
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+        public Node(T value)
+        {
+            Value = value;
         }
     }
 }
